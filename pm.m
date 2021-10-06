281ed5e0739be0fcc51e598fc131bf1d6accc737
Kevin Rasmusson <kevin.rasmusson@hotmail.com>
2021-10-06 23:00:25 +0200

    Finished labeling and plotting.

--- pm.m
+++ pm.m
@@ -7,3 +7,23 @@ S = @(q) q.*6.3775./(3.*10.^9)
 
 % Define demand curve
 D = @(q) exp((log(q) - log(A))/epsilon)
+
+% Let's generate a linspace
+q = linspace(0.001, 4*10^9, 4000000);
+
+% Vectors of values
+Dvalues = D(q);
+Svalues = S(q);
+
+% Set axis values
+axis([0 4*10^9 0 25]);
+
+% Labels please
+title('Price/quantity diagram');
+xlabel('Quantity (10^9 litres)');
+ylabel('Price (kr)');
+
+% Let's plot this
+plot(q, Svalues);
+hold on
+plot(q, Dvalues);
