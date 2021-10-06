3ba66984a9f5c5957bd6db41c0e9bd1834a551a8
Kevin Rasmusson <kevin.rasmusson@hotmail.com>
2021-10-06 22:40:52 +0200

    Added variables and function definitions.

--- pm.m
+++ pm.m
@@ -1,2 +1,9 @@
+% Initiate variables
+epsilon = -0.09
+A = 3*10^9/(17.49^-0.09)
+
 % Define supply curve
 S = @(q) q.*6.3775./(3.*10.^9)
+
+% Define demand curve
+D = @(q) exp((log(q) - log(A))/epsilon)
