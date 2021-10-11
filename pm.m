52cc9712bb2afa8daa24dc1abbd045d33ef124c3
Kevin Rasmusson <kevin.rasmusson@hotmail.com>
2021-10-11 10:37:55 +0200

    Updated variable values and added more.

--- pm.m
+++ pm.m
@@ -1,9 +1,16 @@
+% Clear
+clear
+clc
+
 % Initiate variables
-epsilon = -0.09
-A = 3*10^9/(17.49^-0.09)
+epsilon = -0.09;
+pnotax = 5.98;
+ptax = 15.77;
+qavg = 3.085394.*10.^9;
+A = qavg/(ptax^epsilon)
 
 % Define supply curve
-S = @(q) q.*6.3775./(3.*10.^9)
+S = @(q) q.*pnotax./(qavg)
 
 % Define demand curve
 D = @(q) exp((log(q) - log(A))/epsilon)
@@ -15,13 +22,17 @@ q = linspace(0.001, 4*10^9, 4000000);
 Dvalues = D(q);
 Svalues = S(q);
 
-% Set axis values
-axis([0 4*10^9 0 25]);
+% Initiate figure
+figure
+hold on
 
 % Labels please
 title('Price/quantity diagram');
 xlabel('Quantity (10^9 litres)');
 ylabel('Price (kr)');
+
+% Set axis values
+axis([0 4*10^9 0 25])
 
 % Let's plot this
 plot(q, Svalues);
